c97bb9d97ea0e8559c3a1ce1b295a44c6525b36d
J. M. Alessi <j.m.alessi27@outlook.com>
2022-05-22 20:10:05 -0400

    goals and deploying to heroku

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend/', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production mode.'));
+};
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,7 +9,7 @@ const initialState = {
     isError: false,
     isSuccess: false,
     isLoading: false,
-    message: ''
+    message: '',
 };
 
 // Register user
